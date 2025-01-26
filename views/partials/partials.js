edefd0f5e6fe97b2eee46604565e4d0355afadb0
antoinempinga25 <antoinempinga25@hotmail.com>
2025-01-26 13:45:06 +0200

    W01

---
+++ views/partials/partials.js
@@ -0,0 +1,8 @@
+<head>
+    <meta charset="UTF-8"></meta>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+    <title> CSE 340 App</title>
+    <link rel="stylesheet" href="/css/styles.css"></link>
+</head>
+    
